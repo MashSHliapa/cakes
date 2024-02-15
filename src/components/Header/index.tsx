a2762dbf63e1c6d4da73f139661d06caa801dd68
@MashSHliapa <shliapa.maria@mail.ru>
2024-02-15 14:02:28 +0300

    feat(project): added logo in component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import { MenuBurger } from '../MenuBurger'
 import { Navbar } from '../Navbar'
+import logo from '../images/header_logo.png'
 import './Header.scss'
 
 export function Header() {
   return (
     <header className="header">
       <div className="header__container _container">
-        <div className="header__navbar">
-          <Navbar />
-        </div>
-        <div className="header__burger">
-          <MenuBurger />
+        <div className="header__body">
+          <div className="header__logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="header__navbar">
+            <Navbar />
+          </div>
+          <div className="header__burger">
+            <MenuBurger />
+          </div>
         </div>
       </div>
     </header>
