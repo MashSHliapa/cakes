6941e08a77dac4f1427490264c0c4420ff750570
@MashSHliapa <shliapa.maria@mail.ru>
2024-02-15 21:49:52 +0300

    feat(project): remade component Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,17 +9,7 @@ export function Footer() {
     <footer className="footer">
       <div className="footer__container _container">
         <div className="footer__body">
-          <div className="footer__col footer__col_1">
-            <div className="footer__contacts contacts">
-              <div className="contacts__text _text">Мои контакты:</div>
-              <Contacts />
-              <div className="contacts__text _text">Адрес самовывоза:</div>
-              <div className="contacts__address">г. Минск<br />
-                ул. Белецкого, 14/2</div>
-            </div>
-          </div>
-
-          <div className="footer__col footer__col_2">
+          <div className="footer__col">
             <div className="footer__info info">
               <div className="info__logo">
                 <img src={logo} alt="logo" />
@@ -31,11 +21,23 @@ export function Footer() {
             </div>
           </div>
 
-          <div className="footer__col footer__col_3">
+          <div className="footer__col">
+            <div className="footer__contacts contacts">
+              <div className="contacts__text _text">Мои контакты:</div>
+              <div className="contacts__data">
+                <Contacts />
+              </div>
+              <div className="contacts__text _text">Адрес самовывоза:</div>
+              <div className="contacts__address">г. Минск<br />
+                ул. Белецкого, 14/2</div>
+            </div>
+          </div>
+
+          <div className="footer__col">
             <div className="footer__delivery delivery">
               <div className="delivery__text _text">Способы доставки:</div>
               <div className="delivery__conditions">Возможна доставка через Яндекс <br /> от двери до двери <span>Свадебные заказы от 4 кг.<br />
-                доставляются на место Вашего мероприятия</span> Вы можете забрать свой заказ<br /> по адресу: <br /> г. Минск, ул. Белецкого, 14/2</div>
+                доставляются на место Вашего мероприятия</span> Вы также можете забрать свой заказ самостоятельно! </div>
             </div>
           </div>
         </div>
