f60c231e84c3a4fe6099114fda62e54570869cb2
@MashSHliapa <shliapa.maria@mail.ru>
2024-11-14 21:13:40 +0300

    fix component MainMenu due to design change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,21 +1,20 @@
-import { Header } from '../Header/Header'
-import { Outlet } from 'react-router-dom'
-import { MainMenu } from '../MainMenu'
-import { Footer } from '../Footer/Footer'
-import { Main } from '../Main/Main'
-import './Layout.scss'
-import { Fillings } from '../Fillings/Fillings'
+import { Header } from '../Header/Header';
+import { Outlet } from 'react-router-dom';
+import { MainMenu } from '../MainMenu/MainMenu';
+import { Footer } from '../Footer/Footer';
+import { Main } from '../Main/Main';
+import { Fillings } from '../Fillings/Fillings';
+import './Layout.scss';
 
 export function Layout() {
   return (
     <div className="layout">
       <Header />
       <Main />
-
       <MainMenu />
       <Fillings />
       <Outlet />
       <Footer />
     </div>
-  )
+  );
 }
---
+++ src/components/MainMenu/MainMenu.tsx
@@ -0,0 +1,88 @@
+import forWedding from '../images/mainMenu/for_wedding.png';
+import forChildren from '../images/mainMenu/for_children.png';
+import forChristening from '../images/mainMenu/for_christening.png';
+import twoTier from '../images/mainMenu/two_tier.png';
+import openHoney from '../images/mainMenu/open_honey.png';
+import forMen from '../images/mainMenu/for_men.png';
+import forWomen from '../images/mainMenu/for_women.png';
+import cakeSphere from '../images/mainMenu/cake_sphere.png';
+import pies from '../images/mainMenu/pies.png';
+import sets from '../images/mainMenu/sets.png';
+import logo from '../images/mainMenu/menu_logo.svg';
+import arrow from '../images/mainMenu/menu_arrow.svg';
+import './MainMenu.scss';
+
+export function MainMenu() {
+  return (
+    <div className="main-menu">
+      <div className="main-menu__container _container">
+        <div className="main-menu__body">
+          <div className="main-menu__logo-wrapper">
+            <div className="main-menu__logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <div className="main-menu__separator"></div>
+          </div>
+          <h2 className="main-menu__title">Выберите понравившийся декор из категорий</h2>
+          <ul className="main-menu__list">
+            <li className="main-menu__item">
+              <img src={forWedding} alt="for-wedding" />
+              <div className="main-menu__item-text">Свадебные</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={forChildren} alt="for-children" />
+              <div className="main-menu__item-text">Детские</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={forChristening} alt="for-christening" />
+              <div className="main-menu__item-text">На Крещение</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={twoTier} alt="two-tier" />
+              <div className="main-menu__item-text">Двухярусные</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={openHoney} alt="open_honey" />
+              <div className="main-menu__item-text">Открытые медовики</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={forMen} alt="for_men" />
+              <div className="main-menu__item-text">Для джентльменов</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={forWomen} alt="for_women" />
+              <div className="main-menu__item-text">Для Леди</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={cakeSphere} alt="cake_sphere" />
+              <div className="main-menu__item-text">Торт-сфера</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={pies} alt="pies" />
+              <div className="main-menu__item-text">Пирожные</div>
+            </li>
+            <li className="main-menu__item">
+              <img src={sets} alt="sets" />
+              <div className="main-menu__item-text">Наборы</div>
+            </li>
+          </ul>
+          <div className="main-menu__subtitle">Также можно изменить выбранный декор по Вашему усмотрению</div>
+          <div className="main-menu__fillings-box">
+            <div className="main-menu__fillings-text">Выбирай начинку по своему вкусу</div>
+            <div className="main-menu__arrow">
+              <img src={arrow} alt="arrow" />
+            </div>
+            <div className="main-menu__button-wrapper">
+              <a href="#fillings" className="main-menu__button">
+                Начинки
+              </a>
+            </div>
+          </div>
+          <h3 className="main-menu__slogan">
+            Для создания выбранного Вами угощения используются только натуральные и качественные ингредиенты!
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
